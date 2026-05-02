d6f36339adb1795fdb6e13afcbb7e35676a223b8
DamianB <damian.byrd00@gmail.com>
2026-05-01 18:56:50 -0700

    Add header and main sections

---
+++ src/components/App/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Header from "../Header/Header.jsx";
+import Main from "../Main/Main.jsx";
+
+function App() {
+  return (
+    <div className="page">
+      <div className="page__content">
+        <Header />
+        <Main />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,15 @@
+import "./Main.css";
+import WeatherCard from "../WeatherCard/WeatherCard.jsx";
+
+function Main() {
+  return (
+    <main className="main">
+      <WeatherCard />
+      <section className="cards">
+        <p className="cards__text">Today is 75&deg;F / You may want to wear:</p>
+      </section>
+    </main>
+  );
+}
+
+export default Main;
